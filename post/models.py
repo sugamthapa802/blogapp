6c87149e93b1402bcbb8fb8072dc5f6d2e9dfefa
Sugam Thapa <tsugam74@gmail.com>
2025-11-24 17:37:58 +0545

    change related_names in comment and like model

--- post/models.py
+++ post/models.py
@@ -6,15 +6,15 @@ class Post(models.Model):
     title=models.CharField(max_length=200)
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='posts')
     body=models.TextField(max_length=1000)
-    releveant_pic=models.ImageField(upload_to='posts/',blank=True,null=True)
+    relevant_pic=models.ImageField(upload_to='posts/',blank=True,null=True)
 
     def __str__(self):
         return self.title
 
 class Interactions(models.Model):
-    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='interactions')
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
-    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name='interactions')
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
     class Meta:
         abstract=True
     
